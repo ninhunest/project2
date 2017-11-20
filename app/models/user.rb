dd2c34ef1ef8d7d9b263cc49e2595fd72b5e8b7d
nguyen phan quang ninh <nguyenninhquang@gmail.com>
2017-11-20 16:56:17 +0700

    login and signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   devise :database_authenticatable, :registerable, :recoverable,
-   :rememberable, :trackable, :validatable,:confirmable
+   :rememberable, :trackable, :validatable
 
   validates :name, presence: true,
     length: {maximum: Settings.max_length_name_user}
